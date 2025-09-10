c4ef9f8937d148fa0d04ac5f245e5dbfeaa5dcdf
Matheus95 <matheusgo95ti@gmail.com>
2025-09-09 23:55:11 -0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,11 @@
+const cacheName="furufas-cache-v61b";
+self.addEventListener("install",e=>{
+  e.waitUntil(
+    caches.open(cacheName).then(c=>c.addAll(["index.html"]))
+  )
+});
+self.addEventListener("fetch",e=>{
+  e.respondWith(
+    caches.match(e.request).then(r=>r||fetch(e.request))
+  )
+});
